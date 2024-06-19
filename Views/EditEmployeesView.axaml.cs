7bc76421f9d54260d0cf9a9b9f098e538328807f
GoncharovaNE <12bts.com@gmail.com>
2024-06-19 16:31:05 +0300

    add page in app

---
+++ Views/EditEmployeesView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Acosta.Views;
+
+public partial class EditEmployeesView : UserControl
+{
+    public EditEmployeesView()
+    {
+        InitializeComponent();
+    }
+}
---
+++ Views/EditOutletsView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Acosta.Views;
+
+public partial class EditOutletsView : UserControl
+{
+    public EditOutletsView()
+    {
+        InitializeComponent();
+    }
+}
